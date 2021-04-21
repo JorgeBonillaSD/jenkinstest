83b8816bc3eab8cfaf130dc92098f32fe3860564
Jorge Bonilla <40879445+JorgeBonillaSD@users.noreply.github.com>
2021-04-21 16:45:02 -0700

    Updated python01.py

--- python01.py
+++ python01.py
@@ -5,7 +5,8 @@
 def main():
 
     # this will be displayed to standard out
-    print("You have 13 hours in which to solve the labyrinth...")
+    print("You have 13 hours in which to solve the labyrinth...", end="\n\n")
+    print("Execute this script when the build runs.")
     
 if __name__ == "__main__":
     main()
