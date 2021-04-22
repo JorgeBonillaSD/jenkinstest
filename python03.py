2d5b02c9508db8004f294b644bee9d8a6c71033c
Jorge Bonilla <40879445+JorgeBonillaSD@users.noreply.github.com>
2021-04-21 18:28:16 -0700

    Created python03.py

---
+++ python03.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Russell Zachary Feeser || rzfeeser@alta3.com"""
+
+def main():
+    with open("onISS.txt") as f:
+        print(f.read())
+        
+if __name__ == "__main__":
+    main()
